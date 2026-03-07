607c12d73adfd3c3b9715a595b1d38bdce9c4dd7
Santiago Armas <81274311+shantiaguillo@users.noreply.github.com>
2026-03-07 15:25:53 -0500

    Update app.js
    Corrigiendo JS

--- js/app.js
+++ js/app.js
@@ -3,6 +3,17 @@ const tableBody=document.querySelector("#tradeTable tbody")
 const darkBtn=document.getElementById("darkModeBtn")
 const pdfBtn=document.getElementById("downloadPdfBtn")
 
+const simbolo=document.getElementById("simbolo")
+const fecha=document.getElementById("fecha")
+const hora=document.getElementById("hora")
+const sesion=document.getElementById("sesion")
+const direccion=document.getElementById("direccion")
+const entry=document.getElementById("entry")
+const sl=document.getElementById("sl")
+const tp=document.getElementById("tp")
+const risk=document.getElementById("risk")
+const resultado=document.getElementById("resultado")
+
 let trades=JSON.parse(localStorage.getItem("trades"))||[]
 let editIndex=null
 
@@ -34,12 +45,9 @@ row.innerHTML=`
 <td class="${resultClass}">${trade.resultado}</td>
 
 <td>
-
 <button class="action-btn edit-btn" onclick="editTrade(${index})">Editar</button>
 <button class="action-btn delete-btn" onclick="deleteTrade(${index})">Eliminar</button>
-
 </td>
-
 `
 
 tableBody.appendChild(row)
@@ -55,7 +63,6 @@ form.addEventListener("submit",e=>{
 e.preventDefault()
 
 const trade={
-
 simbolo:simbolo.value,
 fecha:fecha.value,
 hora:hora.value,
@@ -66,24 +73,18 @@ sl:sl.value,
 tp:tp.value,
 risk:risk.value,
 resultado:resultado.value
-
 }
 
 if(editIndex===null){
-
 trades.push(trade)
-
 }else{
-
 trades[editIndex]=trade
 editIndex=null
-
 }
 
 localStorage.setItem("trades",JSON.stringify(trades))
 
 renderTrades()
-
 form.reset()
 
 })
@@ -124,14 +125,10 @@ function applyTheme(){
 const theme=localStorage.getItem("theme")
 
 if(theme==="dark"){
-
 document.body.classList.add("dark-mode")
 darkBtn.textContent="☀️ Clear Mode"
-
 }else{
-
 darkBtn.textContent="🌙 Dark Mode"
-
 }
 
 }
@@ -141,17 +138,13 @@ applyTheme()
 darkBtn.onclick=()=>{
 
 if(document.body.classList.contains("dark-mode")){
-
 document.body.classList.remove("dark-mode")
 localStorage.setItem("theme","light")
 darkBtn.textContent="🌙 Dark Mode"
-
 }else{
-
 document.body.classList.add("dark-mode")
 localStorage.setItem("theme","dark")
 darkBtn.textContent="☀️ Clear Mode"
-
 }
 
 }
